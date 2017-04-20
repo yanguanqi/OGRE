e473e85fbce5b967a179a9bae76695f0d06fae75
闫冠齐 <yanguanqi@aliyun.com>
2017-04-20 22:52:41 +0800

    OgreWikiTutorial_4_UnbufferedInput

---
+++ Visual_Studio/OgreWikiTutorial/OgreProject_Basic_Tutorial_4/TutorialApplication.h
@@ -0,0 +1,43 @@
+/*
+-----------------------------------------------------------------------------
+Filename:    TutorialApplication.h
+-----------------------------------------------------------------------------
+
+This source file is part of the
+   ___                 __    __ _ _    _
+  /___\__ _ _ __ ___  / / /\ \ (_) | _(_)
+ //  // _` | '__/ _ \ \ \/  \/ / | |/ / |
+/ \_// (_| | | |  __/  \  /\  /| |   <| |
+\___/ \__, |_|  \___|   \/  \/ |_|_|\_\_|
+      |___/
+Tutorial Framework (for Ogre 1.9)
+http://www.ogre3d.org/wiki/
+-----------------------------------------------------------------------------
+*/
+
+#ifndef __TutorialApplication_h_
+#define __TutorialApplication_h_
+
+#include "BaseApplication.h"
+
+//---------------------------------------------------------------------------
+
+class TutorialApplication : public BaseApplication
+{
+public:
+    TutorialApplication(void);
+    virtual ~TutorialApplication(void);
+
+protected:
+    virtual void createScene(void);
+	virtual bool frameRenderingQueued(const Ogre::FrameEvent& fe);
+private:
+
+	bool processUnbufferedInput(const Ogre::FrameEvent& fe);
+};
+
+//---------------------------------------------------------------------------
+
+#endif // #ifndef __TutorialApplication_h_
+
+//---------------------------------------------------------------------------
