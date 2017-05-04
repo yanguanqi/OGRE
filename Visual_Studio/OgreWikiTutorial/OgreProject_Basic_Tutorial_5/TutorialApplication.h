adfaf34c9eca57be49240aab1b96bc1562d4ae9c
闫冠齐 <yanguanqi@aliyun.com>
2017-05-04 14:00:45 +0800

    OgreWikiTutorial_5_VS

---
+++ Visual_Studio/OgreWikiTutorial/OgreProject_Basic_Tutorial_5/TutorialApplication.h
@@ -0,0 +1,50 @@
+/*
+-----------------------------------------------------------------------------
+Filename:    TutorialApplication.h
+-----------------------------------------------------------------------------
+
+This source file is part of the
+   ___                 __    __ _ _    _
+  /___\__ _ _ __ ___  / / /\ \ (_) | _(_)
+ //  // _` | '__/ _ \ \ \/  \/ / | |/ / |
+/ \_// (_| | | |  __/  \  /\  /| |   <| |
+\___/ \__, |_|  \___|   \/  \/ |_|_|\_\_|
+      |___/
+Tutorial Framework (for Ogre 1.9)
+http://www.ogre3d.org/wiki/
+-----------------------------------------------------------------------------
+*/
+
+#ifndef __TutorialApplication_h_
+#define __TutorialApplication_h_
+
+#include "BaseApplication.h"
+
+//---------------------------------------------------------------------------
+
+class TutorialApplication : public BaseApplication
+{
+public:
+    TutorialApplication(void);
+    virtual ~TutorialApplication(void);
+
+protected:
+    virtual void createScene(void);
+	virtual bool frameRenderingQueued(const Ogre::FrameEvent& fe);
+	virtual bool keyPressed(const OIS::KeyEvent &arg);
+	virtual bool keyReleased(const OIS::KeyEvent &arg);
+	Ogre::Real mRotate;
+	Ogre::Real mMove;
+	Ogre::SceneNode* mCamNode;
+	Ogre::Vector3 mDirection;
+private:
+	virtual bool mouseMoved(const OIS::MouseEvent &arg);
+	virtual bool mousePressed(const OIS::MouseEvent &arg, OIS::MouseButtonID id);
+	virtual bool mouseReleased(const OIS::MouseEvent &arg, OIS::MouseButtonID id);
+};
+
+//---------------------------------------------------------------------------
+
+#endif // #ifndef __TutorialApplication_h_
+
+//---------------------------------------------------------------------------
